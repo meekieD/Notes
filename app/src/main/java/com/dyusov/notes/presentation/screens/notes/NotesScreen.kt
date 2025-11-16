e7c1c8104637d2eba8ef2279f67305708aff6898
Михаил Дюсов <143973@gmail.com>
2025-11-16 15:03:18 +0300

    3.9 Key и настройка темы

--- app/src/main/java/com/dyusov/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/dyusov/notes/presentation/screens/notes/NotesScreen.kt
@@ -1,15 +1,12 @@
 package com.dyusov.notes.presentation.screens.notes
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -30,7 +27,7 @@ fun NotesScreen(
 
     // Создаем ленивый список (важно не использовать vertical/horizontal scroll!)
     LazyColumn(
-        modifier = Modifier
+        modifier = modifier
             .padding(top = 40.dp),
         // под капотом функция rememberSavable, которая сохраняет значение не только при
         // рекомпозиции @Composable функции, но и при пересоздании Activity
@@ -43,22 +40,32 @@ fun NotesScreen(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                items(state.pinnedNotes) { pinnedNote ->
-                    NotesCard(
-                        note = pinnedNote
-                    )
-                    // последний параметр - лямбда, поэтому можем вне скобок передать действие на клик
-                    {
-                        viewModel.processCommand(
-                            NotesCommand.SwitchPinnedStatus(pinnedNote.id)
+                state.pinnedNotes.forEach { pinnedNote ->
+                    // передаем ключ для связи item и заметки
+                    item(key = pinnedNote.id) {
+                        NotesCard(
+                            note = pinnedNote
                         )
+                        // последний параметр - лямбда, поэтому можем вне скобок передать действие на клик
+                        {
+                            viewModel.processCommand(
+                                NotesCommand.SwitchPinnedStatus(pinnedNote.id)
+                            )
+                        }
                     }
                 }
             }
         }
 
         // также можно использовать функцию items для передачи всей коллекции
-        items(state.otherNotes) { otherNote ->
+        items(
+            items = state.otherNotes,
+            /*
+                функция, которая в качестве параметра принимает заметку
+                и возвращает ключ в зависимости от этой заметки
+            */
+            key = { it.id }
+        ) { otherNote ->
             NotesCard(
                 note = otherNote,
                 // можем передать любое действие на клик
@@ -79,7 +86,7 @@ fun NotesCard(
     onNoteClick: (Note) -> Unit // используем callback
 ) {
     Text(
-        modifier = Modifier.clickable {
+        modifier = modifier.clickable {
             onNoteClick(note)
         },
         text = "${note.title} - ${note.content}",
