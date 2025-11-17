f182eb597ead4df6fe4a65c42acb87646017f1ee
Михаил Дюсов <143973@gmail.com>
2025-11-17 21:58:04 +0300

    3.11 NotesScreen UI. Часть 2

--- app/src/main/java/com/dyusov/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/dyusov/notes/presentation/screens/notes/NotesScreen.kt
@@ -1,14 +1,19 @@
 package com.dyusov.notes.presentation.screens.notes
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -16,6 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -27,7 +33,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dyusov.notes.domain.Note
-import com.dyusov.notes.presentation.ui.theme.Green
+import com.dyusov.notes.presentation.ui.theme.OtherNotesColors
+import com.dyusov.notes.presentation.ui.theme.PinnedNotesColors
 import com.dyusov.notes.presentation.ui.theme.Yellow200
 
 @Composable
@@ -40,20 +47,23 @@ fun NotesScreen(
 
     // Создаем ленивый список (важно не использовать vertical/horizontal scroll!)
     LazyColumn(
-        modifier = modifier
-            .padding(top = 40.dp),
-        // под капотом функция rememberSavable, которая сохраняет значение не только при
-        // рекомпозиции @Composable функции, но и при пересоздании Activity
-        // обычная функция remember {} - только при рекомпозиции
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.padding(top = 48.dp)
     ) {
         // каждый элемент ленивого списка должен быть вызван внутри функции item
         item {
-            Title(text = "All Notes")
+            Title(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "All Notes"
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
         }
 
         item {
             SearchBar(
+                modifier = Modifier.padding(horizontal = 24.dp),
                 query = state.query,
             ) {
                 viewModel.processCommand(NotesCommand.InputSearchQuery(it))
@@ -61,15 +71,28 @@ fun NotesScreen(
         }
 
         item {
-            Subtitle(text = "Pinned")
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+
+        item {
+            Subtitle(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "Pinned"
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
         }
 
         item {
             LazyRow(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                // отступ пропадает при скролле элементов
+                contentPadding = PaddingValues(horizontal = 24.dp)
             ) {
-                state.pinnedNotes.forEach { pinnedNote ->
+                state.pinnedNotes.forEachIndexed { index, pinnedNote ->
                     // передаем ключ для связи item и заметки
                     item(key = pinnedNote.id) {
                         NoteCard(
@@ -89,7 +112,7 @@ fun NotesScreen(
                                     NotesCommand.DeleteNote(pinnedNote.id)
                                 )
                             },
-                            backgroundColor = Yellow200 // цвет заметки
+                            backgroundColor = PinnedNotesColors[index % PinnedNotesColors.size] // цвет заметки
                         )
                     }
                 }
@@ -97,19 +120,33 @@ fun NotesScreen(
         }
 
         item {
-            Subtitle(text = "Others")
+            Spacer(modifier = Modifier.height(24.dp))
         }
 
-        // также можно использовать функцию items для передачи всей коллекции
-        items(
+        item {
+            Subtitle(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "Others"
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
+        // также можно использовать функцию itemsIndexed для передачи всей коллекции + индесы
+        itemsIndexed(
             items = state.otherNotes,
             /*
                 функция, которая в качестве параметра принимает заметку
                 и возвращает ключ в зависимости от этой заметки
             */
-            key = { it.id }
-        ) { otherNote ->
+            key = { _, note -> note.id }
+        ) { index, otherNote ->
             NoteCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
                 note = otherNote,
                 onNoteClick = {
                     viewModel.processCommand(
@@ -126,8 +163,9 @@ fun NotesScreen(
                         NotesCommand.DeleteNote(otherNote.id)
                     )
                 },
-                backgroundColor = Green // цвет заметки
+                backgroundColor = OtherNotesColors[index % OtherNotesColors.size] // цвет заметки
             )
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
@@ -153,7 +191,14 @@ private fun SearchBar(
     onQueryChange: (String) -> Unit
 ) {
     TextField(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                shape = RoundedCornerShape(10.dp)
+            ),
         value = query,
         onValueChange = onQueryChange, // можно и { onQueryChange(it) }
         // placeholder: @Composable (() -> Unit)? -> подход Slot API
@@ -167,12 +212,21 @@ private fun SearchBar(
         // аналогично для иконки поиска
         leadingIcon = {
             Icon(
+                tint = MaterialTheme.colorScheme.onSurface,
                 imageVector = Icons.Default.Search, // иконка поиска по умолчанию
                 contentDescription = "Search notes" // обязательный параметр описания элемента
             )
         },
         // радиус скругления
-        shape = RoundedCornerShape(10.dp)
+        shape = RoundedCornerShape(10.dp),
+        // цвета
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedIndicatorColor = Color.Transparent, // убирает полоску у поиска в фокусе
+            unfocusedIndicatorColor = Color.Transparent, // убирает полоску у поиска вне фокуса
+
+        )
     )
 }
 
@@ -202,7 +256,6 @@ private fun NoteCard(
 ) {
     Column(
         modifier = modifier
-            .fillMaxWidth()
             .clip(RoundedCornerShape(16.dp))
             .background(backgroundColor)
             // для реагирования на разные виды нажатий/кликов
@@ -217,6 +270,8 @@ private fun NoteCard(
                     onDoubleNoteClick(note)
                 }
             )
+            .padding(16.dp),
+
     ) {
         // Заголовок заметки
         Text(
@@ -224,12 +279,18 @@ private fun NoteCard(
             fontSize = 14.sp,
             color = MaterialTheme.colorScheme.onSurface
         )
+        // Отступ между элементами
+        Spacer(modifier = Modifier.height(8.dp))
+
         // Время последнего изменения заметки
         Text(
             text = note.updatedAt.toString(),
             fontSize = 12.sp,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
+        // Отступ между элементами
+        Spacer(modifier = Modifier.height(24.dp))
+
         // Контент заметки
         Text(
             text = note.content,
