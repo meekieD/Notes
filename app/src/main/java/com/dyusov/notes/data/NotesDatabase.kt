738ce405d9a5472ec2e26063913dd63521e81c50
Михаил Дюсов <143973@gmail.com>
2025-11-25 18:56:40 +0300

    3.26 Создание базы данных

---
+++ app/src/main/java/com/dyusov/notes/data/NotesDatabase.kt
@@ -0,0 +1,37 @@
+package com.dyusov.notes.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// Room создаст за нас БД, поэтому класс абстрактный
+@Database(
+    entities = [NoteDbModel::class], // таблицы в БД, требуется список Kotlin классов
+    version = 1,
+    exportSchema = false // не нужна история версий БД (false)
+)
+abstract class NotesDatabase : RoomDatabase() {
+
+    abstract fun notesDao(): NotesDao
+
+    companion object {
+        private val LOCK = Any()
+        private var instance: NotesDatabase? = null
+
+        fun getInstance(context: Context): NotesDatabase {
+            instance?.let { return it }
+
+            synchronized(LOCK) {
+                instance?.let { return it }
+                return Room.databaseBuilder(
+                    context = context,
+                    klass = NotesDatabase::class.java, // здесь требуется Java класс
+                    name = "notes.db"
+                ).build().also {
+                    instance = it // кладем созданный экземпляр БД в переменную instance
+                }
+            }
+        }
+    }
+}
